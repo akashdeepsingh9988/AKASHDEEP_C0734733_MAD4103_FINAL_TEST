b28c4e7f63e901df531a94e89a9f1141ee488b70
Akashdeep Singh <akashthind007@gmail.com>
2018-11-01 07:48:48 -0400

    SQL LITE PLUGIN ADDED

--- AKASHDEEP_C0734733_MAD4103_FINAL_TEST/platforms/ios/www/js/index.js
+++ AKASHDEEP_C0734733_MAD4103_FINAL_TEST/platforms/ios/www/js/index.js
@@ -1,46 +1,102 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var db = null;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+// add event listeners
+document.addEventListener("deviceReady", connectToDatabase);
+document.getElementById("saveButton").addEventListener("click", saveButtonPressed);
+document.getElementById("showAllButton").addEventListener("click", showAllPressed)
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function connectToDatabase() {
+  console.log("device is ready - connecting to database");
+  if (window.cordova.platformId === 'browser') {
+    db = window.openDatabase("cestar", "1.0", "Database for Cestar College app", 2*1024*1024);
+  }
+  else {
+    var databaseDetails = {"name":"cestar.db", "location":"default"}
+    db = window.sqlitePlugin.openDatabase(databaseDetails);
+    console.log("done opening db");
+  }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  if (!db) {
+    alert("databse connection failed!");
+    return false;
+  }
 
-        console.log('Received Event: ' + id);
+  // 3. create relevant tables
+  db.transaction(createTables)
+
+}
+
+function createTables(transaction) {
+  var sql = "CREATE TABLE IF NOT EXISTS employee (id integer PRIMARY KEY AUTOINCREMENT, name text, dept text)"
+  //var sql = "CREATE TABLE IF NOT EXISTS employee (name text, dept text)"
+  transaction.executeSql(sql, [], createSuccess, createFail)
+}
+
+function createSuccess(tx, result) {
+
+}
+function createFail(error) {
+  alert("Failure while creating table: " + error);
+}
+
+
+
+
+function saveButtonPressed(transaction) {
+  console.log("save!!!");
+  // get name and department from the user interface
+  var n = document.getElementById("name").value;
+  var d = document.getElementById("dept").value;
+
+  db.transaction(function (transaction) {
+      // save the values to the database
+      var sql = "INSERT INTO employee (name, dept) VALUES (?,?)";
+      
+      
+      transaction.executeSql(sql,[n,d], function(tx,result){
+        alert("Insert success: " + JSON.stringify(result));
+        showAllPressed()
+      }, function(error){
+        alert("Insert failed: " + error);
+      });
     }
-};
+  );
 
-app.initialize();
+}
+
+function showAllPressed() {
+  // clear the user interface
+  document.getElementById("dbItems").innerHTML = "";
+
+  db.transaction(function(transaction) {
+    transaction.executeSql("SELECT * FROM employee", [],
+      function (tx, results) {
+        var numRows = results.rows.length;
+
+        for (var i = 0; i < numRows; i++) {
+
+          // to get individual items:
+          var item = results.rows.item(i);
+          console.log(item);
+          console.log(item.name);
+
+          // show it in the user interface
+          document.getElementById("dbItems").innerHTML +=
+              "<p>Name: " + item.name + "</p>"
+            + "<p>Dept: " + item.dept + "</p>"
+            + "<p>=======================</p>";
+        }
+
+      }, function(error){
+      });
+  });
+}
+
+/* DELETE statement
+db.transaction(function(txn) {
+  txn.executeSql("DROP TABLE employee",[],
+      function(tx,results){console.log("Successfully Dropped")},
+      function(tx,error){console.log("Could not delete")}
+  );
+});
+*/
