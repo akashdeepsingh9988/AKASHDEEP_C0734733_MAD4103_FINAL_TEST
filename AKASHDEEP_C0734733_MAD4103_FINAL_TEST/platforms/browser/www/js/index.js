92089a48a951b3fb6c2a5def7e2fad7457664bb4
Akashdeep Singh <akashthind007@gmail.com>
2018-11-01 09:02:01 -0400

    hero availability done

--- AKASHDEEP_C0734733_MAD4103_FINAL_TEST/platforms/browser/www/js/index.js
+++ AKASHDEEP_C0734733_MAD4103_FINAL_TEST/platforms/browser/www/js/index.js
@@ -6,82 +6,91 @@ document.getElementById("insert-hero").addEventListener("click", saveButtonPress
 document.getElementById("show-heros").addEventListener("click", showAllPressed)
 
 function connectToDatabase() {
-  console.log("device is ready - connecting to database");
-  if (window.cordova.platformId === 'browser') {
-    db = window.openDatabase("superb", "1.0", "Database for super rescue agency app", 2*1024*1024);
-  }
-  else {
-    var databaseDetails = {"name":"superb.db", "location":"default"}
-    db = window.sqlitePlugin.openDatabase(databaseDetails);
-    console.log("done opening db");
-  }
+    console.log("device is ready - connecting to database");
+    if (window.cordova.platformId === 'browser') {
+        db = window.openDatabase("superb", "1.0", "Database for super rescue agency app", 2 * 1024 * 1024);
+    } else {
+        var databaseDetails = {"name": "superb.db", "location": "default"}
+        db = window.sqlitePlugin.openDatabase(databaseDetails);
+        console.log("done opening db");
+    }
 
-  if (!db) {
-    alert("databse connection failed!");
-    return false;
-  }
+    if (!db) {
+        alert("databse connection failed!");
+        return false;
+    }
 
-  // 3. create relevant tables
-  db.transaction(createTables)
+    // 3. create relevant tables
+    db.transaction(createTables)
 
 }
 
 function createTables(transaction) {
-  var sql = "CREATE TABLE IF NOT EXISTS heroes (id integer PRIMARY KEY AUTOINCREMENT, name text, isAvailable integer)";
-  transaction.executeSql(sql, [], createSuccess, createFail)
+    var sql = "CREATE TABLE IF NOT EXISTS heroes (id integer PRIMARY KEY AUTOINCREMENT, name text, isAvailable integer)";
+    transaction.executeSql(sql, [], createSuccess, createFail)
 }
 
 function createSuccess(tx, result) {
 
 }
 function createFail(error) {
-  alert("Failure while creating table: " + error);
+    alert("Failure while creating table: " + error);
 }
 
 
 
 
 function saveButtonPressed(transaction) {
-  console.log("save!!!");
-  db.transaction(function (transaction) {
-      // save the values to the database
-      var sql = "INSERT INTO heroes (name, isAvailable) VALUES ('Spiderman',1), ('Thor',1), ('Captain America',0), ('Wonder Women',0)";
-      
-      transaction.executeSql(sql,[], function(tx,result){
-        alert("Insert success: " + JSON.stringify(result));
-        showAllPressed()
-      }, function(error){
-        alert("Insert failed: " + error);
-      });
+    console.log("save!!!");
+    db.transaction(function (transaction) {
+        // save the values to the database
+        var sql = "INSERT INTO heroes (name, isAvailable) VALUES ('Spiderman',1), ('Thor',1), ('Captain America',0), ('Wonder Women',0)";
+
+        transaction.executeSql(sql, [], function (tx, result) {
+            alert("Insert success: " + JSON.stringify(result));
+            //showAllPressed()
+        }, function (error) {
+            alert("Insert failed: " + error);
+        });
     }
-  );
+    );
 
 }
 
 function showAllPressed() {
-  // clear the user interface
-  document.getElementById("dbItems").innerHTML = "";
+    // clear the user interface
+    document.getElementById("dbItems").innerHTML = "";
 
-  db.transaction(function(transaction) {
-    transaction.executeSql("SELECT * FROM heroes", [],
-      function (tx, results) {
-        var numRows = results.rows.length;
+    db.transaction(function (transaction) {
+        transaction.executeSql("SELECT * FROM heroes", [],
+                function (tx, results) {
+                    var numRows = results.rows.length;
 
-        for (var i = 0; i < numRows; i++) {
+                    for (var i = 0; i < numRows; i++) {
 
-          // to get individual items:
-          var item = results.rows.item(i);
-          console.log(item);
-          console.log(item.name);
+                        // to get individual items:
+                        var item = results.rows.item(i);
+                        console.log(item);
+                        console.log(item.name);
+                        var av = "";
+                        if (item.isAvailable == 1)
+                        {
+                            av = "Yes";
+                        } 
+                        else
+                        {
+                            av = "No";
 
-          // show it in the user interface
-          document.getElementById("dbItems").innerHTML +=
-              "<p>Name: " + item.name + "</p>"
-            + "<p>Available for help : " + item.isAvailable + "</p>"
-            + "<p>=======================</p>";
-        }
+                        }
 
-      }, function(error){
-      });
-  });
+                        // show it in the user interface
+                        document.getElementById("dbItems").innerHTML +=
+                                "<p>Name: " + item.name + "</p>"
+                                + "<p>Available  To Hire : " + av + "</p>"
+                                + "<p>=======================</p>";
+                    }
+
+                }, function (error) {
+        });
+    });
 }
